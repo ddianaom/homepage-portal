ffd5e5d36355a672d2117de36067b867ce180be0
Diana de Oliveira Martins <99148060+ddianaom@users.noreply.github.com>
2024-09-24 09:38:44 -0400

    Add Footer, News, Banner

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,8 @@ function moveSlide(direction) {
     const slides = document.querySelectorAll('.carousel-item');
     const totalSlides = slides.length;
 
-    // Atualiza o índice do slide
     currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
 
-    // Move o carrossel
     const carouselSlide = document.querySelector('.carousel-slide');
     carouselSlide.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
